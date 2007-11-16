328dd69621703bbef2a712bbdfd37d2807dc5bec
sami.nyc <sami.nyc@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2007-11-16 20:48:34 +0000

    Changed spatial point filter to be a sub class of new PolygonSpatialPointFilter.

--- trunk/src/main/java/org/shapelogic/filter/PointAboveFilter.java
+++ trunk/src/main/java/org/shapelogic/filter/PointAboveFilter.java
@@ -7,25 +7,14 @@ import org.shapelogic.polygon.IPoint2D;
  * @author Sami Badawi
  *
  */
-public class PointAboveFilter extends PolygonPointFilter {
-	private double _minLimit = Double.NaN; 
-
+public class PointAboveFilter extends PolygonSpatialPointFilter {
+	
 	@Override
 	public boolean evaluate(Object obj) {
 		IPoint2D point = (IPoint2D) obj;
-		if (_minLimit == Double.NaN) 
+		if (_limit == Double.NaN) 
 			return true;
 		else 
-			return _minLimit <= point.getY();
-	}
-	
-	@Override
-	public void setup() throws Exception {
-		if (_constraint instanceof String)
-			_minLimit = Double.parseDouble((String)_constraint); 
-		else if (_constraint instanceof Number)
-			_minLimit = ((Number)_constraint).doubleValue();
-		if (_minLimit != Double.NaN) 
-			_minLimit *= getParent().getBBox().getDiagonalVector().getY();
+			return _limit <= point.getY();
 	}
 }
