a593d2203fb156ca9e43e71e157318fc7fd73b7c
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2008-01-30 17:23:45 +0000

    Test for calc InContexts.

---
+++ src/test/java/org/shapelogic/calculation/QueryCalcTest.java
@@ -0,0 +1,43 @@
+package org.shapelogic.calculation;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+import junit.framework.TestCase;
+
+/** Test QueryCalc.
+ * 
+ * @author Sami Badawi
+ *
+ */
+public class QueryCalcTest extends TestCase {
+	private IQueryCalc<String, Integer> query;
+	private Map<String, Integer> mapDirectValues;
+	private Map<String, Calc<Integer> > mapLazyCalcValues;
+	private String KEY = "version";
+	private Integer VALUE1 = 1;
+	private Integer VALUE2 = 2;
+	private CalcFixed<Integer> ONE = new CalcFixed<Integer>(VALUE2); 
+	private String DOUBLE_KEY = "dependent";
+	
+	@Override
+	public void setUp() {
+		query = new QueryCalc<String, Integer>();
+		mapDirectValues = new TreeMap<String, Integer>(); 
+		mapLazyCalcValues = new TreeMap<String, Calc<Integer> >(); 
+		mapDirectValues.put(KEY, VALUE1);
+		mapLazyCalcValues.put(KEY, ONE);
+	}
+
+	public void test() {
+		assertNotNull(query);
+		assertEquals(VALUE1,query.get(KEY, mapDirectValues));
+
+		assertEquals(VALUE2,query.get(KEY, mapLazyCalcValues));
+		
+		assertEquals(VALUE1,query.get(KEY, mapLazyCalcValues, mapDirectValues));
+		
+		assertEquals(VALUE2,query.get(KEY, mapDirectValues, mapLazyCalcValues));
+
+	}
+}
