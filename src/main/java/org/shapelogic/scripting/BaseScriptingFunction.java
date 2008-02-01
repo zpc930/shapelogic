343d996f7b61b35f02be057139eaa2d6ca75d205
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2008-02-01 12:27:23 +0000

    Took out _name and generic types.

--- src/main/java/org/shapelogic/scripting/BaseScriptingFunction.java
+++ src/main/java/org/shapelogic/scripting/BaseScriptingFunction.java
@@ -1,19 +1,15 @@
 package org.shapelogic.scripting;
 
 import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
 
 /** BaseScriptingFunction base of any scripting functions.
  * 
  * @author Sami Badawi
  *
  */
-public abstract class BaseScriptingFunction<In,Out> {
+public abstract class BaseScriptingFunction {
 
 	public static final String DEFAULT_LANGUAGE = "groovy";
-	public static final String FUNCTION_NAME_SUFFIX = "_FUNCTION_";
-	
-	protected String _name;
 	protected String _functionName;
 	protected String _expression;
 	protected String _language = DEFAULT_LANGUAGE;
@@ -27,7 +23,6 @@ public abstract class BaseScriptingFunction<In,Out> {
 			ScriptEngine engine;
 			try {
 				ScriptEngineFactory.script(_expression, _language);
-				_scriptEngine.put(_name, this);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
