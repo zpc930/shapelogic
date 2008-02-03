3579b78a98aff44ba4814cb8ed722a48cfc11d21
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2008-02-03 04:16:41 +0000

    Add OnePredicateSelectorTest.

---
+++ src/test/java/org/shapelogic/predicate/OnePredicateSelectorTest.java
@@ -0,0 +1,79 @@
+package org.shapelogic.predicate;
+
+import org.shapelogic.scripting.FunctionPredicate;
+import org.shapelogic.util.Constants;
+
+import junit.framework.TestCase;
+
+/** Test OnePredicateSelector.
+ * <br />
+ * 
+ * @author Sami Badawi
+ *
+ */
+public class OnePredicateSelectorTest extends TestCase {
+	NamedPredicate<Integer> even;
+	NamedPredicate<Integer> thirds;
+	NamedPredicate<Integer>[] predicates;
+	OnePredicateSelector<Integer> xorEvenThirds;
+	Predicate<Integer> evenScript;
+	
+	public void setUpJava() {
+		even = new NamedPredicate<Integer>() {
+			@Override
+			public boolean evaluate(Integer input) {
+				return input % 2 == 0;
+			}
+			@Override
+			public String getOhName() {
+				return "even";
+			}
+		};
+		
+		thirds = new NamedPredicate<Integer>() {
+			@Override
+			public boolean evaluate(Integer input) {
+				return input % 3 == 0;
+			}
+			@Override
+			public String getOhName() {
+				return "thirds";
+			}
+		};
+		
+		predicates = new NamedPredicate[] {even, thirds};
+		xorEvenThirds = new OnePredicateSelector<Integer>(predicates);
+	}
+	
+	public void setUpGroovy() {
+		evenScript = new FunctionPredicate<Integer>("even", "def even = {it%2 == 0};",Constants.GROOVY);
+		even = new NamedPredicateDecorator<Integer>(evenScript, "even"); 
+		
+		thirds = new NamedPredicateDecorator<Integer>(new FunctionPredicate<Integer>("thirds", "def thirds = {it%3 == 0};",Constants.GROOVY), "thirds");
+		
+		predicates = new NamedPredicate[] {even, thirds};
+		xorEvenThirds = new OnePredicateSelector<Integer>(predicates);
+	}
+	
+	public void testJava() {
+		setUpJava();
+		common();
+	}
+
+	public void testGroovy() {
+		setUpGroovy();
+		common();
+	}
+
+	public void common() {
+		assertTrue(even.evaluate(0));
+		assertFalse(even.evaluate(1));
+		assertNull(xorEvenThirds.transform(0));
+		assertNull(xorEvenThirds.transform(1));
+		assertEquals("even",xorEvenThirds.transform(2));
+		assertEquals("thirds",xorEvenThirds.transform(3));
+		assertEquals("even",xorEvenThirds.transform(4));
+		assertNull(xorEvenThirds.transform(5));
+		assertNull(xorEvenThirds.transform(6));
+	}
+}
