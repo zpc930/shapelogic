ebad2b03672c57e91d1114bb659ddf6d3ebe73ac
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2008-02-03 04:15:57 +0000

    Added FunctionPredicate using Scripting to define predicate.

---
+++ src/main/java/org/shapelogic/scripting/FunctionPredicate.java
@@ -0,0 +1,55 @@
+package org.shapelogic.scripting;
+
+import javax.script.Invocable;
+import javax.script.ScriptException;
+
+import org.shapelogic.predicate.Predicate;
+
+/** Transform based on an expression in a Scripting language using JSR 223.
+ *  
+ * Requires Groovy to be installed. Need special installation of groovy-engine.jar 
+ * that need to be downloade from Sun.
+ * 
+ * Requires that the scripting language support the invocable interface.
+ * 
+ * @author Sami Badawi
+ *
+ */
+public class FunctionPredicate<In> extends BaseScriptingFunction
+	implements Predicate<In> 
+{
+	public FunctionPredicate(String functionName, String expression, String language) {
+		_functionName = functionName;
+		if (language != null)
+			_language = language;
+		_expression = expression;
+	}
+	
+	public FunctionPredicate(String name, String expression){
+		this(name,expression,DEFAULT_LANGUAGE);
+	}
+	
+	public Object invoke(In input) {
+		Invocable inv = (Invocable)getScriptEngine();
+		if (inv == null)
+			return null;
+	    Object obj = null;
+		try {
+			obj = inv.invokeFunction(_functionName, new Object[] { input });
+		} catch (ScriptException e) {
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		}
+	    return obj;
+	}
+
+	@Override
+	public boolean evaluate(In input) {
+		Object result = invoke(input);
+		if (result == null) return false;
+		if (result instanceof Boolean) return ((Boolean)result).booleanValue();
+		if (result instanceof Number) return ((Number)result).doubleValue() == 0.0;
+		return false;
+	}
+}
