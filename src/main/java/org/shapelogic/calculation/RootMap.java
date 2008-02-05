b37da0af9d05a3557831a7fd2d8cb666e0aca866
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2008-02-05 17:13:01 +0000

    Added Rootmap.

---
+++ src/main/java/org/shapelogic/calculation/RootMap.java
@@ -0,0 +1,48 @@
+package org.shapelogic.calculation;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/** This is the RootContext where all the certain objects are stored. <br />
+ * 
+ * There will be a lot of calculations and streams here. <br />
+ * 
+ * Currently implemented as a kind of a singleton. <br />
+ * This might be changed if more contexts need to be active at the same time.<br />
+ * 
+ * This survives between tests so reset if testing.
+ * 
+ * @author Sami Badawi
+ *
+ */
+public class RootMap {
+	private static final boolean NEW_MAP_ON_CLEAR = false;
+	static Map _map = new HashMap();
+	static Map[] _maps = {_map};
+	
+	static public Map getMap() {
+		return _map;
+	}
+	
+	static public Map[] getMaps(){
+		return _maps;
+	}
+	
+	static public void put(Object key, Object value) {
+		_map.put(key, value);
+	}
+	
+	static public Object get(Object key) {
+		return _map.get(key);
+	}
+	
+	/** Maybe making a new map would be better. */
+	static public void clear() {
+		if (NEW_MAP_ON_CLEAR) {
+			_map = new HashMap();
+			_maps[0] = _map;
+		}
+		else
+			_map.clear();
+	}
+}
