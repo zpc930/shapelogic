2b7127a5a62daf15c3324f63732bb2bb39b9fa74
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2008-02-05 17:26:53 +0000

    It is a little problematic there are 2 calculation that need to be made. So they cannot both have the same method name: invoke(input) Change comments.

--- trunk/src/test/java/org/shapelogic/streams/BaseListFilterTransformerStreamTest.java
+++ trunk/src/test/java/org/shapelogic/streams/BaseListFilterTransformerStreamTest.java
@@ -1,43 +1,46 @@
 package org.shapelogic.streams;
 
+import java.util.Map.Entry;
+
+import org.shapelogic.calculation.Calc1;
 import org.shapelogic.mathematics.NaturalNumberStream;
 
 import junit.framework.TestCase;
 
-/** Test BaseListFilterTransformerStream.
+/** Test BaseListFilterTransformerStream.<br />
+ * 
+ * BaseListFilterTransformerStreamTest and BaseListFilterTransformerStreamTest are the same.
  * 
  * @author Sami Badawi
  *
  */
 public class BaseListFilterTransformerStreamTest extends TestCase {
-	BaseListFilterTransformerStream<Integer,Integer> evenNumbers;
+	NumberedStream<Entry<Integer,Integer> > evenNumbers;
 	
 	@Override
 	public void setUp() {
-		NaturalNumberStream naturalNumbers = new NaturalNumberStream();
+		NaturalNumberStream naturalNumbers = new NaturalNumberStream(5);
 		evenNumbers = new BaseListFilterTransformerStream<Integer,Integer>(naturalNumbers){
-
 			@Override
 			public Integer transform(Integer input) {
 				if (input % 2 == 0)
 					return input;
 				return null;
 			}
-
 		};
 	} 
 	
 	public void testWithNext(){
 		assertEquals(new Integer(0),evenNumbers.next().getKey());
 		assertEquals(new Integer(2),evenNumbers.next().getKey());
-//		assertEquals(new Integer(4),evenNumbers.next().getKey());
-//		assertNull(evenNumbers.next());
+		assertEquals(new Integer(4),evenNumbers.next().getKey());
+		assertNull(evenNumbers.next());
 	}
 
 	public void testWithGet(){
 		assertEquals(new Integer(0),evenNumbers.get(0).getKey());
 		assertEquals(new Integer(2),evenNumbers.get(1).getKey());
-//		assertEquals(new Integer(4),evenNumbers.get(2).getKey());
+		assertEquals(new Integer(4),evenNumbers.get(2).getKey());
 //		assertNull(evenNumbers.get(3));
 	}
 
