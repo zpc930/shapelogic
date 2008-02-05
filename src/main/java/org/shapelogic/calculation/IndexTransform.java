776fe09ebca322683d869f27922f8cc5593444bb
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2008-02-05 17:47:42 +0000

    Deprecate IndexTransform.

--- src/main/java/org/shapelogic/calculation/IndexTransform.java
+++ src/main/java/org/shapelogic/calculation/IndexTransform.java
@@ -5,11 +5,14 @@ package org.shapelogic.calculation;
  * So it will take the same index from both the input and the output.<br /> 
  * It will do outSeq.get(index) = transform(inSeq.get(index),index).<br /> 
  * 
+ * Deprecated instead use Calc1.<br />
+ * 
  * @author Sami Badawi
  *
  * @param <In> Type of input
  * @param <Out> Type of output
  */
+@Deprecated
 public interface IndexTransform<In,Out> {
 	Out transform(In input, int index);
 }
