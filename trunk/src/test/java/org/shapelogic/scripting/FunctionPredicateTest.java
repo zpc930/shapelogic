3e9ca9dd3775ef1a1c1dcfd9c3255a69c5136ec6
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2008-02-10 20:04:31 +0000

    Changed to inplement both Calc1 and Predicate.

--- trunk/src/test/java/org/shapelogic/scripting/FunctionPredicateTest.java
+++ trunk/src/test/java/org/shapelogic/scripting/FunctionPredicateTest.java
@@ -14,7 +14,7 @@ public class FunctionPredicateTest extends TestCase {
 
 	public void testGroovyFunctionEven() throws Exception {
 		Predicate<Integer> evenPredicate = 
-			new FunctionPredicate<Integer>("even", "def even = {it%2 == 0};",Constants.GROOVY);
+			new FunctionPredicate<Integer>("evenGroovy", "def evenGroovy = {it%2 == 0};",Constants.GROOVY);
 		assertTrue(evenPredicate.evaluate(0));
 		assertFalse(evenPredicate.evaluate(1));
 		assertTrue(evenPredicate.evaluate(2));
