f30da2e9d5e3bed026bb50d88707b86c91d146a2
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2008-02-18 02:29:13 +0000

    Added ListCalcStream1 and test class.

---
+++ src/main/java/org/shapelogic/streams/ListCalcStream1.java
@@ -0,0 +1,36 @@
+package org.shapelogic.streams;
+
+import org.shapelogic.calculation.Calc1;
+import org.shapelogic.util.Constants;
+
+/** Make a stream that is using a Calc1 class to do a calculation.
+ * <br />
+ * In type does not seem to be used.<br />
+ * 
+ * @author Sami Badawi
+ * 
+ */
+public class ListCalcStream1<In, E> extends BaseListStream1<In, E> {
+	protected Calc1<In, E> _calc;
+	
+	public ListCalcStream1(Calc1<In, E> calc, NumberedStream<In> inputStream, int maxLast) {
+		super(inputStream, maxLast);
+		_calc = calc;
+	}
+
+	public ListCalcStream1(Calc1<In, E> calc, NumberedStream<In> inputStream) {
+		this(calc,inputStream, Constants.LAST_UNKNOWN);
+	}
+
+	public ListCalcStream1() {
+	}
+	
+	public ListCalcStream1(Calc1<In, E> calc, int maxLast) {
+		this(calc,null,maxLast);
+	}
+
+	@Override
+	public E invoke(In input) {
+		return _calc.invoke(input);
+	}
+}
