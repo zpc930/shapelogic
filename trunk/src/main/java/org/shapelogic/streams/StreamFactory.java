d8284a7758514129cf511e923696589b9e4f32f3
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2008-02-18 02:33:53 +0000

    Updated StreamFactory.

--- trunk/src/main/java/org/shapelogic/streams/StreamFactory.java
+++ trunk/src/main/java/org/shapelogic/streams/StreamFactory.java
@@ -2,10 +2,10 @@ package org.shapelogic.streams;
 
 import java.util.Iterator;
 
+import org.shapelogic.calculation.Calc1;
 import org.shapelogic.calculation.CalcIndex1;
 import org.shapelogic.predicate.BinaryPredicate;
-import org.shapelogic.predicate.Predicate;
-import org.shapelogic.scripting.FunctionCalcIndex0;
+import org.shapelogic.scripting.FunctionCalc1;
 import org.shapelogic.scripting.FunctionCalcIndex1;
 import org.shapelogic.util.Constants;
 
@@ -16,17 +16,17 @@ import org.shapelogic.util.Constants;
  */
 public class StreamFactory {
 	static public <In, E> ListStream<E> createListStream(CalcIndex1<In, E> transformer){
-		return new TransformerListStream<In, E>(transformer);
+		return new ListCalcIndexStream1<In, E>(transformer);
 	}
 
 	static public <E> ListStream<E> createListStream(Iterator<E> iterator, int maxLast){
-		ListStream<E> result = new TransformerListStream<Object, E>(createCalcIndex1(iterator));
+		ListStream<E> result = new ListCalcIndexStream1<Object, E>(createCalcIndex1(iterator));
 		result.setMaxLast(maxLast);
 		return result;
 	}
 	
 	static public <E> ListStream<E> createListStream(Iterator<E> iterator){
-		return new TransformerListStream<Object, E>(createCalcIndex1(iterator));
+		return new ListCalcIndexStream1<Object, E>(createCalcIndex1(iterator));
 	}
 	
 	static public <E> CalcIndex1<Object, E>  createCalcIndex1(final Iterator<E> iterator) {
@@ -56,7 +56,7 @@ public class StreamFactory {
 	static public <In,E> ListStream<E> createListStream(String name, String expression , String language, Integer stopNumber, E ... startList){
 		String functionName = name + Constants.FUNCTION_NAME_SUFFIX;
 		FunctionCalcIndex1<In, E> transformer = new FunctionCalcIndex1<In, E>(functionName, expression, language); 
-		ListStream<E> result = new TransformerListStream<In, E>(transformer);
+		ListStream<E> result = new ListCalcIndexStream1<In, E>(transformer);
 		if (stopNumber != null)
 			result.setMaxLast(stopNumber);
 		for (E el: startList)
@@ -66,12 +66,12 @@ public class StreamFactory {
 	}
 	
 	/** Create stream based on a function.
-	 * 
-	 * I need to handle something like this:
-	 * 
-	 * A, "polygons", "def tempA2 = {it.points.length}", "==", 5
-	 * 
-	 * What should the output type of this be?
+	 * <br />
+	 * I need to handle something like this:<br />
+	 * <br />
+	 * A, "polygons", "def tempA2 = {it.points.length}", "==", 5<br />
+	 * <br />
+	 * What should the output type of this be?<br />
 	 * NumberedStream[Boolean] <br />
 	 * CaclIndex[Boolean] <br />
 	 * Predicate[Integer] <br />
@@ -86,26 +86,28 @@ public class StreamFactory {
 	 * @param language Scripting language by name, Groovy is the default
 	 * @return
 	 */
-	static public <In0, In1> ListStream<Boolean> createListStream0(String name, 
+	static public <In0, In1, In2> ListStream<Boolean> createListStream0(String name, 
 			String inputName, String expression, 
-			BinaryPredicate<In0, In1> binaryPredicate, In1 compareObject, String language)
+			final BinaryPredicate<In1, In2> binaryPredicate, final In2 compareObject, String language)
 	{
 		String functionName = name + Constants.FUNCTION_NAME_SUFFIX;
 		//I need to insert the transformer in the stream otherwise I have a 
 		//problem with an extra stream and what kind of caching I should use
 		//would it be better to have a way to turn an expression into a Calc and 
 		//I should have a way to combine Calcs.
-		FunctionCalcIndex0<In0> transformer = 
-			new FunctionCalcIndex0<In0>(functionName, expression, language);
-//		transformer
-//		ListStream<E> result = new TransformerListStream<In, E>(transformer);
-//		if (stopNumber != null)
-//			result.setMaxLast(stopNumber);
-//		for (E el: startList)
-//			result.getList().add(el);
-//		transformer.put(name, result);
-//		return result;
-		return null;
+//		final 
+		final NamedNumberedStream0<In0> input = new NamedNumberedStream0(inputName);
+		final FunctionCalc1<In0,In1> calc1 = 
+			new FunctionCalc1<In0,In1>(functionName, expression, language);
+		final Calc1<In0,Boolean> calcBoolean = new Calc1<In0,Boolean>() {
+			@Override
+			public Boolean invoke(In0 input) {
+				return binaryPredicate.evaluate(calc1.invoke(input), compareObject);
+			}
+		};
+		final ListCalcStream1<In0,Boolean> calcStream1 = 
+			new ListCalcStream1<In0,Boolean>(calcBoolean,input);
+		return calcStream1;
 	}
 	
 }
