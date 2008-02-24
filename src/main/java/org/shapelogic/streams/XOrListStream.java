00d3c03a2a441eebb2daf22949a72c09e499ba6a
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2008-02-24 04:27:24 +0000

    Add XOr stream.

---
+++ src/main/java/org/shapelogic/streams/XOrListStream.java
@@ -0,0 +1,60 @@
+package org.shapelogic.streams;
+
+import java.util.List;
+
+import org.shapelogic.util.Constants;
+
+/** AndListStream takes a list of Boolean streams and create the and of them.
+ * 
+ * @author Sami Badawi
+ *
+ */
+public class XOrListStream extends BaseListStreamList<Boolean,String> {
+	
+	/** Parallel to the NumberedStream. */
+	protected List<String> _ohNames;
+	
+	/** Use the ohName to also be the name of the input stream. <br />
+	 * 
+	 * @param ohNames
+	 * @param maxLast
+	 */
+	public XOrListStream(List<String> ohNames, int maxLast) {
+		super(null,maxLast);
+		_ohNames = ohNames;
+		for (String streamName: _ohNames) {
+			NumberedStream numberedStream = new NamedNumberedStream0(streamName);
+			this.getInputStream().add(numberedStream);
+		}
+	}
+	
+	public XOrListStream(List<String> ohNames, List<NumberedStream<Boolean> > inputStream, int maxLast) {
+		super(inputStream, maxLast);
+		_ohNames = ohNames;
+	}
+	
+	public XOrListStream(List<String> ohNames, List<NumberedStream<Boolean> > inputStream) {
+		this(ohNames,inputStream,Constants.LAST_UNKNOWN);
+	}
+	
+	@Override
+	public String invoke(List<Boolean> input) {
+		int i = 0;
+		String foundName = null;
+		int found = 0;
+		for (Boolean bool : input) {
+			if (bool) {
+				found++;
+				if (1 < found )
+					return Constants.NO_OH;
+				else
+					foundName = _ohNames.get(i);
+			}
+			i++;
+		}
+		if (1 == found)
+			return foundName;
+		return Constants.NO_OH;
+	}
+
+}
