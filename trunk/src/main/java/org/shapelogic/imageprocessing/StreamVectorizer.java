bcd58d07b77750f4936288c0339916fbdf5c5900
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2008-02-28 21:58:24 +0000

    Split MaxDistanceVectorizer in 2 the old and a new where the logic is based on Stream.

---
+++ trunk/src/main/java/org/shapelogic/imageprocessing/StreamVectorizer.java
@@ -0,0 +1,10 @@
+package org.shapelogic.imageprocessing;
+
+/** Same vectorizer as MaxDistanceVectorizer, but logic implemented with streams.
+ * <br />
+ * 
+ * @author Sami Badawi
+ *
+ */
+public class StreamVectorizer extends BaseMaxDistanceVectorizer {
+}
