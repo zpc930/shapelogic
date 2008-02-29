9f02e2fee996c3a6b37dad277676e0c05f019b58
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2008-02-29 16:15:01 +0000

    Renamed class.

---
+++ src/main/java/org/shapelogic/streams/NamedNumberedStream.java
@@ -0,0 +1,79 @@
+package org.shapelogic.streams;
+
+import org.shapelogic.calculation.RootMap;
+import org.shapelogic.util.Constants;
+
+/** Get a Named Stream out of the context.
+ * 
+ * @author Sami Badawi
+ *
+ */
+public class NamedNumberedStream<E> implements NumberedStream<E> {
+	protected String _key;
+	protected NumberedStream<E> _inputStream; 
+	
+	public NamedNumberedStream(String key, int maxLast) {
+		_key = key;
+		_inputStream = (NumberedStream) RootMap.get(_key);
+	}
+
+	public NamedNumberedStream(String key) {
+		this(key,Constants.LAST_UNKNOWN);
+	}
+
+	@Override
+	public E get(int input) {
+		return _inputStream.get(input);
+	}
+
+	@Override
+	public int getIndex() {
+		return _inputStream.getIndex();
+	}
+
+	@Override
+	public int getLast() {
+		return _inputStream.getLast();
+	}
+
+	@Override
+	public int getMaxLast() {
+		return _inputStream.getMaxLast();
+	}
+
+	@Override
+	public void setMaxLast(int maxLast) {
+		_inputStream.setMaxLast(maxLast);
+	}
+
+	@Override
+	public boolean isDirty() {
+		return _inputStream.isDirty();
+	}
+
+	@Override
+	public void setup() {
+		_inputStream.setup();
+	}
+
+	@Override
+	public E getValue() {
+		return _inputStream.getValue();
+	}
+
+	@Override
+	public boolean hasNext() {
+		return _inputStream.hasNext();
+	}
+
+	@Override
+	public E next() {
+		return _inputStream.next();
+	}
+
+	@Override
+	public void remove() {
+		_inputStream.remove();
+	}
+	
+}
