9db0e8a2311e84fd0e782bdd808f97edeec00405
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2008-02-29 17:23:10 +0000

    Functionalety for testing if setup has been called.

---
+++ trunk/src/main/java/org/shapelogic/calculation/SetupFlagged.java
@@ -0,0 +1,12 @@
+package org.shapelogic.calculation;
+
+/** To test if something has been setup.
+ * 
+ * @author Sami Badawi
+ *
+ */
+public interface SetupFlagged {
+	
+	boolean isSetup();
+	void setup();
+}
