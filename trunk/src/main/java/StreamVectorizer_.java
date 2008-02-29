3ce2005b90a1ed3035e87807dc67ec7cbc1b9e49
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2008-02-29 15:22:19 +0000

    Added StreamVectorizer

---
+++ trunk/src/main/java/StreamVectorizer_.java
@@ -0,0 +1,17 @@
+import org.shapelogic.imageprocessing.StreamVectorizer;
+
+/** Thin wrapper around StreamVectorizer.
+ * <br />
+ * Third approach to a vectorizer. This work well.
+ * 
+ * @author Sami Badawi
+ *
+ */
+public class StreamVectorizer_ extends StreamVectorizer {
+
+	@Override
+	public boolean isGuiEnabled() {
+		return true;
+	}
+
+}
