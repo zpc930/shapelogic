9230f8750a39e2038df569b4aaf1c94e6935a890
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2008-03-04 01:07:48 +0000

    Added more polygon streams.

--- src/main/java/org/shapelogic/streamlogic/LoadPolygonStreams.java
+++ src/main/java/org/shapelogic/streamlogic/LoadPolygonStreams.java
@@ -2,6 +2,10 @@ package org.shapelogic.streamlogic;
 
 import static org.shapelogic.logic.CommonLogicExpressions.*;
 
+import java.util.List;
+import java.util.Set;
+
+import org.hsqldb.lib.Collection;
 import org.shapelogic.calculation.Calc1;
 import org.shapelogic.calculation.RootMap;
 import org.shapelogic.polygon.Polygon;
@@ -45,7 +49,7 @@ public class LoadPolygonStreams {
 	 * <br />
 	 * In order for this to work the polygons have to be defined first.
 	 */
-	public static void loadStreamsRequiredForLetterMatch(NumberedStream<Polygon> polygons) {
+	public static void loadStreamsRequiredForStraightLetterMatch(NumberedStream<Polygon> polygons) {
 		
 		loadPointCountStream(polygons);
 		
@@ -58,6 +62,33 @@ public class LoadPolygonStreams {
 		loadEndPointCountStream(polygons);
 	}
 
+	/** Load all the required streams for the letter matcher to work.
+	 * <br />
+	 * In order for this to work the polygons have to be defined first.
+	 */
+	public static void loadStreamsRequiredForLetterMatch(NumberedStream<Polygon> polygons) {
+		
+		loadStreamsRequiredForStraightLetterMatch(polygons);
+		
+		loadHoleCountStream(polygons);
+		
+		loadSoftPointCountStream(polygons);
+		
+		loadHardPointCountStream(polygons);
+		
+		loadUJunctionPointCountStream(polygons);
+		
+		loadTJunctionPointCountStream(polygons);
+
+		loadCurveArchCountStream(polygons);
+		
+		multiLineCountStream(polygons);
+		
+		aspectRatioStream(polygons);
+		
+		loadInflectionPointCountStream(polygons);
+	}
+	
 	public static void loadStreamsRequiredForLetterMatch() {
 		//In order for this to work the polygons have to be defined first
 		NumberedStream<Polygon> polygons = new NamedNumberedStreamLazySetup<Polygon>(StreamNames.POLYGONS);
@@ -126,4 +157,129 @@ public class LoadPolygonStreams {
 		RootMap.put(POINT_COUNT,pointCountStream);
 	}
 	
+	private static void loadHoleCountStream(NumberedStream<Polygon> polygons) {
+		Calc1<Polygon, Integer> holeCountCalc1 = new Calc1<Polygon, Integer>() {
+			@Override
+			public Integer invoke(Polygon input) {
+				return input.getHoleCount();
+			}
+		};
+		ListStream<Integer> holeCountStream = 
+			new ListCalcStream1<Polygon, Integer>(holeCountCalc1,polygons); 
+		RootMap.put(HOLE_COUNT,holeCountStream);
+	}
+	
+	private static void loadSoftPointCountStream(NumberedStream<Polygon> polygons) {
+		Calc1<Polygon, Integer> softPointCountCalc1 = new Calc1<Polygon, Integer>() {
+			@Override
+			public Integer invoke(Polygon input) {
+				return size(input.getAnnotatedShape().getShapesForAnnotation("PointType.SOFT_POINT"));
+			}
+		};
+		ListStream<Integer> softPointCountStream = 
+			new ListCalcStream1<Polygon, Integer>(softPointCountCalc1,polygons); 
+		RootMap.put(SOFT_POINT_COUNT,softPointCountStream);
+	}
+	
+	private static void loadHardPointCountStream(NumberedStream<Polygon> polygons) {
+		Calc1<Polygon, Integer> hardPointCountCalc1 = new Calc1<Polygon, Integer>() {
+			@Override
+			public Integer invoke(Polygon input) {
+				return size(input.getAnnotatedShape().getShapesForAnnotation("PointType.HARD_CORNER"));
+			}
+		};
+		ListStream<Integer> hardPointCountStream = 
+			new ListCalcStream1<Polygon, Integer>(hardPointCountCalc1,polygons); 
+		RootMap.put(HARD_POINT_COUNT,hardPointCountStream);
+	}
+	
+	private static void loadUJunctionPointCountStream(NumberedStream<Polygon> polygons) {
+		Calc1<Polygon, Integer> uJunctionPointCountCalc1 = new Calc1<Polygon, Integer>() {
+			@Override
+			public Integer invoke(Polygon input) {
+				return size(input.getAnnotatedShape().getShapesForAnnotation("PointType.U_JUNCTION"));
+			}
+		};
+		ListStream<Integer> uJunctionPointCountStream = 
+			new ListCalcStream1<Polygon, Integer>(uJunctionPointCountCalc1,polygons); 
+		RootMap.put(U_JUNCTION_POINT_COUNT,uJunctionPointCountStream);
+	}
+	
+	private static void loadTJunctionPointCountStream(NumberedStream<Polygon> polygons) {
+		Calc1<Polygon, Integer> tJunctionPointCountCalc1 = new Calc1<Polygon, Integer>() {
+			@Override
+			public Integer invoke(Polygon input) {
+				return size(input.getAnnotatedShape().getShapesForAnnotation("PointType.T_JUNCTION"));
+			}
+		};
+		ListStream<Integer> tJunctionPointCountStream = 
+			new ListCalcStream1<Polygon, Integer>(tJunctionPointCountCalc1,polygons); 
+		RootMap.put(T_JUNCTION_POINT_COUNT,tJunctionPointCountStream);
+	}
+	
+	private static void loadCurveArchCountStream(NumberedStream<Polygon> polygons) {
+		Calc1<Polygon, Integer> curveArchCountCalc1 = new Calc1<Polygon, Integer>() {
+			@Override
+			public Integer invoke(Polygon input) {
+				return size(input.getAnnotatedShape().getShapesForAnnotation("LineType.CURVE_ARCH"));
+			}
+		};
+		ListStream<Integer> curveArchCountStream = 
+			new ListCalcStream1<Polygon, Integer>(curveArchCountCalc1,polygons); 
+		RootMap.put(CURVE_ARCH_COUNT,curveArchCountStream);
+	}
+	
+	private static void multiLineCountStream(NumberedStream<Polygon> polygons) {
+		Calc1<Polygon, Integer> curveArchCountCalc1 = new Calc1<Polygon, Integer>() {
+			@Override
+			public Integer invoke(Polygon input) {
+				return size(input.getMultiLines());
+			}
+		};
+		ListStream<Integer> curveArchCountStream = 
+			new ListCalcStream1<Polygon, Integer>(curveArchCountCalc1,polygons); 
+		RootMap.put(MULTI_LINE_COUNT,curveArchCountStream);
+	}
+	
+	private static void aspectRatioStream(NumberedStream<Polygon> polygons) {
+		Calc1<Polygon, Number> aspectRatioCalc1 = new Calc1<Polygon, Number>() {
+			@Override
+			public Number invoke(Polygon input) {
+				return input.getBBox().getAspectRatio();
+			}
+		};
+		ListStream<Number> aspectRatioStream = 
+			new ListCalcStream1<Polygon, Number>(aspectRatioCalc1,polygons); 
+		RootMap.put(ASPECT_RATIO,aspectRatioStream);
+	}
+	
+	private static void loadInflectionPointCountStream(NumberedStream<Polygon> polygons) {
+		Calc1<Polygon, Integer> inflectionPointCountCalc1 = new Calc1<Polygon, Integer>() {
+			@Override
+			public Integer invoke(Polygon input) {
+				return size(input.getAnnotatedShape().getShapesForAnnotation("LineType.INFLECTION_POINT"));
+			}
+		};
+		ListStream<Integer> inflectionPointCountStream = 
+			new ListCalcStream1<Polygon, Integer>(inflectionPointCountCalc1,polygons); 
+		RootMap.put(INFLECTION_POINT_COUNT,inflectionPointCountStream);
+	}
+	
+	static public int size(Collection collection) {
+		if (collection == null)
+			return 0;
+		return collection.size();
+	}
+
+	static public int size(List collection) {
+		if (collection == null)
+			return 0;
+		return collection.size();
+	}
+
+	static public int size(Set set) {
+		if (set == null)
+			return 0;
+		return set.size();
+	}
 }
