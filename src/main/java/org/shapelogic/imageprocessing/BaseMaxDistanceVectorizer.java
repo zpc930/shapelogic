bbb16ad2bda155c21b9505c9238aa8f8ff720f85
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2008-03-05 02:20:47 +0000

    Add html to Java doc.

--- src/main/java/org/shapelogic/imageprocessing/BaseMaxDistanceVectorizer.java
+++ src/main/java/org/shapelogic/imageprocessing/BaseMaxDistanceVectorizer.java
@@ -9,14 +9,15 @@ import org.shapelogic.polygon.Calculator2D;
 import org.shapelogic.util.Constants;
 
 /** Vectorizer that is splitting lines based on max distance to line between end points.
- * 
+ * <br />
+ * <p>
  * The main idea is that this will read a whole multi line at a time.
  * Then later it will split it according to max distance of pixels to the line 
  * between start and end point of the multi line.
- * 
+ * </p> <p>
  * Maybe this could be completely abstracted out, maybe but at that point I 
  * will just take most of this class and turn it into a base class.
- * 
+ * </p> <p>
  * Always stop on junctions, if there is one junction point use that, but stop after.
  * N points are chosen last.
  * Never go from one N point to another, 
@@ -27,25 +28,25 @@ import org.shapelogic.util.Constants;
  * neighbors first. 
  * I might have to know how many N points there are if there are more just
  * add all to _unfinishedPoints. 
- * 
+ * </p> <p>
  * Treatment of different points:
  * Junction: add to new point, move to first junction. 
  * N points: count, keep track of first.
  * Other: count, keep track of first.
  * Unused: count, keep track of first. I think that is already done.
  * Used: count, keep track of first.
- * 
+ * </p> <p>
  * For each junction add to unfinished. Go to first junction.
  * If other points are available take first and go to it.
  * If only N point is available, if current point an N and not the first point 
  * stop else go to that.
- * 
+ * </p> <p>
  * When coming to a new point check if it is a junction if stop if not on 
  * first point. It does not matter if the start point is used or not.
  * I think that at the end check to see if you can go to either a junction 
  * point or to the start point.
  * Also stop if you do not know what to do, at the end of handleProblematicPoints().
- * 
+ * </p>
  * @author Sami Badawi
  *
  */
