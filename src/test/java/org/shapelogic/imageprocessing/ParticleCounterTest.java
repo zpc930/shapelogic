c4b0459aab92dbbd4ce26a34518d78bdc76bde5d
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2008-03-27 16:28:35 +0000

    Added ParticleCounterTest.

---
+++ src/test/java/org/shapelogic/imageprocessing/ParticleCounterTest.java
@@ -0,0 +1,66 @@
+package org.shapelogic.imageprocessing;
+
+import ij.process.ByteProcessor;
+import ij.process.ColorProcessor;
+import ij.process.ImageProcessor;
+
+/** Test SBSegment.
+ * <br />
+ * 
+ * @author Sami Badawi
+ *
+ */
+public class ParticleCounterTest extends AbstractImageProcessingTests {
+	ParticleCounter _segmenter = new ParticleCounter();
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		_dirURL = "./src/test/resources/images/particles";
+		_fileFormat = ".gif";
+	}
+	
+	public void testShortVerticalGif() {
+		String fileName = "oneWhitePixelGray";
+		ImageProcessor bp = runPluginFilterOnImage(filePath(fileName), _segmenter);
+		assertEquals(1,bp.getWidth());
+		assertTrue(bp instanceof ByteProcessor);
+		int pixel = bp.get(0,0);
+		assertEquals(0,pixel);
+		PixelAreaFactory factory = _segmenter.getSegmentation().getSegmentAreaFactory();
+		assertNotNull(factory);
+		assertEquals(1,factory.getStore().size());
+		assertTrue(_segmenter.isParticleImage());
+	}
+
+	/** This shows that when you save and image as PNG it will always be opened 
+	 * as a color image.
+	 */
+	public void testShortVerticalPng() {
+		String fileName = "oneWhitePixelGray";
+		ImageProcessor bp = runPluginFilterOnImage(filePath(fileName,".png"), _segmenter);
+		assertEquals(1,bp.getWidth());
+		assertTrue(bp instanceof ColorProcessor);
+		int pixel = bp.get(0,0);
+		assertEquals(-1,pixel);
+		PixelAreaFactory factory = _segmenter.getSegmentation().getSegmentAreaFactory();
+		assertNotNull(factory);
+		assertEquals(1,factory.getStore().size());
+		assertTrue(_segmenter.isParticleImage());
+	}
+
+	public void testBlobsGif() {
+		String fileName = "blobs";
+		ImageProcessor bp = runPluginFilterOnImage(filePath(fileName), _segmenter);
+		assertEquals(256,bp.getWidth());
+		assertEquals(65024,bp.getPixelCount());
+		assertTrue(bp instanceof ByteProcessor);
+		int pixel = bp.get(0,0);
+		assertEquals(40,pixel);
+		PixelAreaFactory factory = _segmenter.getSegmentation().getSegmentAreaFactory();
+		assertNotNull(factory);
+		assertEquals(9761,factory.getStore().size()); 
+		assertFalse(_segmenter.isParticleImage()); //XXX should be changed to true
+	}
+
+}
