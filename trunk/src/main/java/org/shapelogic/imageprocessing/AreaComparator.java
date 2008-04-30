0e291d61d21011c084fb2aade7fcf728f12f80bf
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2008-04-30 13:43:47 +0000

    Now works on HasArea interface.

--- trunk/src/main/java/org/shapelogic/imageprocessing/AreaComparator.java
+++ trunk/src/main/java/org/shapelogic/imageprocessing/AreaComparator.java
@@ -2,17 +2,18 @@ package org.shapelogic.imageprocessing;
 
 import java.util.Comparator;
 
-import org.shapelogic.color.ValueArea;
+//import org.shapelogic.color.ValueArea;
+import org.shapelogic.imageutil.HasArea;
 
 /** Compare area by pixel size. <br />
  * 
  * @author Sami Badawi
  *
  */
-public class AreaComparator implements Comparator<ValueArea> {
+public class AreaComparator implements Comparator<HasArea> {
 
 	@Override
-	public int compare(ValueArea o1, ValueArea o2) {
+	public int compare(HasArea o1, HasArea o2) {
 		return o1.getArea() - o2.getArea();
 	}
 
