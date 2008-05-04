7222fbbf802edcf2248bbcbc03840c815d33f063
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2008-05-04 01:57:53 +0000

    Made ParticleCounter and SegmentCounter independent.

--- src/main/java/org/shapelogic/imageprocessing/ParticleCounter.java
+++ src/main/java/org/shapelogic/imageprocessing/ParticleCounter.java
@@ -2,7 +2,10 @@ package org.shapelogic.imageprocessing;
 
 import java.util.List;
 
-import org.shapelogic.color.ValueArea;
+import org.shapelogic.color.IColorAndVariance;
+import org.shapelogic.imageutil.BaseImageOperation;
+
+import static org.shapelogic.imageutil.ImageJConstants.*;
 
 /** ParticleCounter count number of particles in a particle image.
  * <br />
@@ -12,13 +15,64 @@ import org.shapelogic.color.ValueArea;
  * @author Sami Badawi
  *
  */
-public class ParticleCounter extends SegmentCounter {
+public class ParticleCounter extends BaseImageOperation {
 	
 	Boolean _particleImage;
+	protected boolean _doAll = true;
+	
+	/** Modifying colors */
+	protected boolean _modifying = true;
+	protected SBSegmentation _segmentation;
+	protected String _pluginName = "Segmenter";
+	protected boolean _saveArea;
+	
+	public void run() {
+//		if (!(ip instanceof ByteProcessor || ip instanceof ColorProcessor))
+//			return;
+		try {
+			int startX = getImage().getWidth()/2;
+			int startY = getImage().getHeight()/2;
+			SBSimpleCompare compare = SBSimpleCompare.factory(getImage());
+			compare.grabColorFromPixel(startX, startY);
+			compare.setModifying(_modifying);
+			_segmentation = new SBSegmentation();
+			_segmentation.setSLImage(getImage());
+			_segmentation.setPixelCompare(compare);
+			if (_saveArea)
+				_segmentation.setSegmentAreaFactory(SBSimpleCompare.segmentAreaFactory(getImage()));
+			_segmentation.init();
+			if (_doAll)
+				_segmentation.segmentAll();
+			else
+				_segmentation.segment(startX, startY);
+			showMessage(_pluginName, getStatus());
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public String getStatus() {
+		String status = _segmentation.getStatus();
+		String negation = "";
+		if (!isParticleImage())
+			negation = "not ";
+		status += "\nImage is " + negation + "a particle image.";
+		if (isParticleImage()) {
+			status += "\nParticle count: " + getParticleCount();
+		}
+		return status;
+	}
+
+	public SBSegmentation getSegmentation() {
+		return _segmentation;
+	}
+
 	
 	public ParticleCounter()
 	{
-		super(true);
+		super(DOES_8G+DOES_RGB+DOES_STACKS+SUPPORTS_MASKING);
+		_saveArea = true;
 		_modifying = false;
 		_pluginName = "Particle Counter";
 	}
@@ -26,7 +80,7 @@ public class ParticleCounter extends SegmentCounter {
 	public boolean isParticleImage() {
 		if (_particleImage == null) {
 			_segmentation.getSegmentAreaFactory().sort();
-			List<ValueArea> store = _segmentation.getSegmentAreaFactory().getStore();
+			List<IColorAndVariance> store = _segmentation.getSegmentAreaFactory().getStore();
 			int biggestArea = store.get(store.size()-1).getArea();
 			double biggestAreaPercentage = biggestArea * 100 / getSegmentation().getSLImage().getPixelCount();
 			_particleImage = biggestAreaPercentage > 50;
@@ -36,17 +90,5 @@ public class ParticleCounter extends SegmentCounter {
 
 	public int getParticleCount() {
 		return _segmentation.getSegmentAreaFactory().getStore().size() - 1;
-	}
-
-	public String getStatus() {
-		String status = super.getStatus();
-		String negation = "";
-		if (!isParticleImage())
-			negation = "not ";
-		status += "\nImage is " + negation + "a particle image.";
-		if (isParticleImage()) {
-			status += "\nParticle count: " + getParticleCount();
-		}
-		return status;
 	}
 }
