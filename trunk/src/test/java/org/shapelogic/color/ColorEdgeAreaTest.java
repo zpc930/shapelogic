fab8769aa7ababa31a56ce1280b53ae746f9faf1
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2008-05-06 21:11:06 +0000

    More work on ParticleCounter. Fixed bug in PixelArea.

--- trunk/src/test/java/org/shapelogic/color/ColorEdgeAreaTest.java
+++ trunk/src/test/java/org/shapelogic/color/ColorEdgeAreaTest.java
@@ -12,6 +12,7 @@ public class ColorEdgeAreaTest extends TestCase {
 	
 	public void test1Points() {
 		ColorEdgeArea grayArea = new ColorEdgeArea(10,20,100);
+		grayArea.putPixel(10, 20, 100);
 		assertEquals(1, grayArea.getArea());
 		assertEquals(10., grayArea.getPixelArea().getCenterPoint().getX());
 		assertEquals(20., grayArea.getPixelArea().getCenterPoint().getY());
@@ -24,6 +25,7 @@ public class ColorEdgeAreaTest extends TestCase {
 
 	public void test2Points() {
 		ColorEdgeArea grayArea = new ColorEdgeArea(10,20,100);
+		grayArea.putPixel(10, 20, 100);
 		grayArea.putPixel(40, 30, 0);
 		assertEquals(2, grayArea.getArea());
 		assertEquals(25., grayArea.getPixelArea().getCenterPoint().getX());
