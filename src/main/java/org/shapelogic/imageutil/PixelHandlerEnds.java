4e386334f1c17897ff0450695579fd3a6ca98a05
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2008-05-24 18:05:56 +0000

    Updated PixelHandler classes.

--- src/main/java/org/shapelogic/imageutil/PixelHandlerEnds.java
+++ src/main/java/org/shapelogic/imageutil/PixelHandlerEnds.java
@@ -1,6 +1,14 @@
 package org.shapelogic.imageutil;
 
+/** PixelHandler with hooks for calling method at the beginning and ends of each
+ * line. <br />
+ * 
+ * @author Sami Badawi
+ */
 public interface PixelHandlerEnds extends PixelHandler {
+    
+    /** Called before starting to scan the image. */
+    void setup();
 	
 	/** Handle a pixel with a color and a coordinate. */
 	void handlePixelStart(int x, int y, int color);
@@ -8,4 +16,6 @@ public interface PixelHandlerEnds extends PixelHandler {
 	/** Handle a pixel with a color and a coordinate. */
 	void handlePixelEnd(int x, int y, int color);
 	
+    /** Called after scan of the image. */
+    void postProcess();
 }
