e9c0a7d39b0df8cf0373ddcf9a6a1039b8284cf6
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2008-05-26 02:54:25 +0000

    Added new way to do ColorReplacer.

--- src/main/java/org/shapelogic/imageprocessing/ColorReplacePixelHandler.java
+++ src/main/java/org/shapelogic/imageprocessing/ColorReplacePixelHandler.java
@@ -4,39 +4,41 @@ import org.shapelogic.color.ColorDistance1;
 import org.shapelogic.color.ColorDistance1RGB;
 import org.shapelogic.color.IColorDistance;
 import org.shapelogic.imageutil.HasSLImage;
-import org.shapelogic.imageutil.PixelHandlerEnds;
+import org.shapelogic.imageutil.PixelHandler;
 import org.shapelogic.imageutil.SLImage;
 
 /** Handles Color Replace for individual pixel, used with wrapper to work on whole image.<br />
- *
+ * 
+ * Example of a PixelHandler that ca be used to change a whole image.<br />
+ * 
  * @author Sami Badawi
  */
-public class ColorReplacePixelHandler implements PixelHandlerEnds, HasSLImage {
-    protected static double _maxDistance = 15.0;
-    protected static int _referenceColor = 1;
+public class ColorReplacePixelHandler implements PixelHandler, HasSLImage {
+    //Direct simple parameters are not just public fields without getters and setters.
+    public static double maxDistance = 15.0;
+    public static int referenceColor = 1;
     protected IColorDistance _colorDistance;
     protected SLImage _image;
 
-    public ColorReplacePixelHandler() {
-    }
-    
     public void setup() {
         if (_image == null || _image.isEmpty())
             return;
-        if (_image.isRgb()) {
-        	_colorDistance = new ColorDistance1RGB();
+        if (_colorDistance == null) {
+            if (_image.isRgb()) {
+                _colorDistance = new ColorDistance1RGB();
+            }
+            else {
+                _colorDistance = new ColorDistance1();
+            }
         }
-        else {
-        	_colorDistance = new ColorDistance1();
-        }
-        _colorDistance.setReferenceColor(_referenceColor);
+        _colorDistance.setReferenceColor(referenceColor);
     }
 
     @Override
     public void putPixel(int x, int y, int color) {
         int currentColor = _image.get(x, y);
-        if (_colorDistance.distanceToReferenceColor(currentColor) < _maxDistance) {
-            _image.set(x, y,_referenceColor);
+        if (_colorDistance.distanceToReferenceColor(currentColor) < maxDistance) {
+            _image.set(x, y,referenceColor);
         }
     }
     
@@ -48,37 +50,9 @@ public class ColorReplacePixelHandler implements PixelHandlerEnds, HasSLImage {
         this._colorDistance = _colorDistance;
     }
 
-    public static double getMaxDistance() {
-        return _maxDistance;
-    }
-
-    public static void setMaxDistance(double maxDistance) {
-        ColorReplacePixelHandler._maxDistance = maxDistance;
-    }
-
-    public static int getReferenceColor() {
-        return _referenceColor;
-    }
-
-    public static void setReferenceColor(int referenceColor) {
-        _referenceColor = referenceColor;
-    }
-
     @Override
     public void setImage(SLImage image) {
         _image = image;
         setup();
-    }
-
-    @Override
-    public void handlePixelStart(int x, int y, int color) {
-    }
-
-    @Override
-    public void handlePixelEnd(int x, int y, int color) {
-    }
-
-    @Override
-    public void postProcess() {
     }
 }
