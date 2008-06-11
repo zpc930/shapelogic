b7d1dd656a6093b9dc05e98063733c593bdff60d
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2008-06-11 22:37:02 +0000

    Added input dialog box for parameters and results table.

--- trunk/src/main/java/ParticleCounter_.java
+++ trunk/src/main/java/ParticleCounter_.java
@@ -1,5 +1,19 @@
+import ij.IJ;
+import ij.ImagePlus;
+import ij.gui.GenericDialog;
+import ij.measure.ResultsTable;
+import ij.plugin.filter.ExtendedPlugInFilter;
+import ij.plugin.filter.PlugInFilterRunner;
+import ij.process.ImageProcessor;
+
+import java.util.List;
+
+import org.shapelogic.color.IColorAndVariance;
 import org.shapelogic.imageprocessing.ParticleCounter;
-import org.shapelogic.imageutil.PlugInFilterAdapter;
+import org.shapelogic.imageutil.IJGui;
+import org.shapelogic.imageutil.IJImage;
+import org.shapelogic.imageutil.PixelArea;
+import org.shapelogic.imageutil.SLImage;
 
 /** Automatic particle counter for 24 bit RGB and 8 bit Gray.
  * <br />
@@ -10,10 +24,74 @@ import org.shapelogic.imageutil.PlugInFilterAdapter;
  * @author Sami Badawi
  *
  */
-public class ParticleCounter_ extends PlugInFilterAdapter {
+public class ParticleCounter_ extends ParticleCounter implements ExtendedPlugInFilter {
+
+	static final String COLOR = "Color";
 	
-	public ParticleCounter_() {
-		super(new ParticleCounter());
+    protected ResultsTable _rt = new ResultsTable();
+    protected GenericDialog _gd;
+    
+	@Override
+	public void run(ImageProcessor ip) {
+		run();
+		populateTable();
+	}
+
+	@Override
+	public int setup(String arg, ImagePlus imp) {
+		_guiWrapper = IJGui.INSTANCE;
+		if (imp == null)
+			return setup(arg, (SLImage)null);
+		return setup(arg, new IJImage(imp));
+		
+	}
+	
+    void populateTable() {
+    	
+    	List<IColorAndVariance> particles = _segmentation.getSegmentAreaFactory().getStore();
+    	_rt.getFreeColumn(COLOR);
+    	int i=0;
+    	_rt.setDefaultHeadings();
+    	for (IColorAndVariance particle: particles) {
+        	if (particle == null)
+        		continue;
+        	if (particle.getArea() < getMinPixelsInArea())
+        		continue;
+        	_rt.incrementCounter();
+        	_rt.addValue(ResultsTable.AREA, particle.getArea());
+        	_rt.addValue(ResultsTable.STD_DEV, particle.getStandardDeviation());
+        	_rt.addValue(COLOR, particle.getMeanColor());
+        	PixelArea pixelArea = particle.getPixelArea();
+        	if (pixelArea != null) {
+            	_rt.addValue(ResultsTable.X_CENTER_OF_MASS, pixelArea.getCenterPoint().getX());
+            	_rt.addValue(ResultsTable.Y_CENTER_OF_MASS, pixelArea.getCenterPoint().getY());
+        	}
+    		i++;
+    	}
+    	_rt.show("Particle properties");
+    }
+
+	@Override
+	public void setNPasses(int passes) {
+	}
+
+	@Override
+	public int showDialog(ImagePlus imp, String command, PlugInFilterRunner pfr) {
+        _iterations = 3;
+        _maxDistance = 70;
+        _minPixelsInArea = 10;
+        _gd = new GenericDialog(getClass().getSimpleName(), IJ.getInstance());
+        _gd.addNumericField("Max distance: ", _maxDistance, 0);
+        _gd.addNumericField("Min pixels: ", _minPixelsInArea, 0);
+        _gd.addNumericField("Min pixels: ", _iterations, 0);
+        _gd.showDialog();
+        if (_gd.wasCanceled()) {
+            return DONE;
+        }
+        _maxDistance = _gd.getNextNumber();
+        _minPixelsInArea = (int)_gd.getNextNumber();
+        _iterations = (int)_gd.getNextNumber();
+        return IJ.setupDialog(imp, _setupReturnValue);
 	}
 }
 
