3b606def0c0dc5ca54f3f5bd0c9ecc84f905820a
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2008-06-22 10:54:38 +0000

    Added class containing headings for results tables.

---
+++ src/main/java/org/shapelogic/util/Headings.java
@@ -0,0 +1,14 @@
+package org.shapelogic.util;
+
+/** Class containing headings for results tables.<br />
+ * 
+ * @author Sami Badawi
+ *
+ */
+public class Headings {
+
+	//XXX move to a constants or headings class
+	public static final String COLOR = "Color";
+	public static final String PERIMETER = "Perimeter";
+
+}
