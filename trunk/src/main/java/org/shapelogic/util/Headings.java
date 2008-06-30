b4ce2b354f08771cfa2878671d1a147c0dc3c02f
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2008-06-30 22:01:14 +0000

    Added lots of color particle analyzer headings.

--- trunk/src/main/java/org/shapelogic/util/Headings.java
+++ trunk/src/main/java/org/shapelogic/util/Headings.java
@@ -12,5 +12,13 @@ public class Headings {
 	public static final String PERIMETER = "Perimeter";
 	public static final String CIRCULARITY = "Circularity";
 	public static final String ASPECT_RATIO = "AspectRatio";
+	public static final String GRAY_VALUE = "GrayValue";
+	public static final String BOUNDING_BOX_X_MIN = "xMin";
+	public static final String BOUNDING_BOX_Y_MIN = "yMin";
+	public static final String BOUNDING_BOX_X_MAX = "xMax";
+	public static final String BOUNDING_BOX_Y_MAX = "yMax";
+	public static final String HARD_CORNERS = "HardCorners";
+	public static final String INFLECTION_POINT_COUNT = "InflectionPoints";
+	public static final String CURVE_ARCH_COUNT = "CurveArchs";
 
 }
