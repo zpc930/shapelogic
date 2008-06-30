b82c0876f2a6ded894cd58c975dc575f93912fc7
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2008-06-30 22:00:13 +0000

    Added color stream names.

--- trunk/src/main/java/org/shapelogic/streamlogic/StreamNames.java
+++ trunk/src/main/java/org/shapelogic/streamlogic/StreamNames.java
@@ -10,5 +10,9 @@ public class StreamNames {
 	public static final String POLYGONS = "polygons";
 	public static final String LETTERS = "letters";
 	public static final String PARTICLES = "particles";
+	static public String COLOR_R = "colorR";
+	static public String COLOR_G = "colorG";
+	static public String COLOR_B = "colorB";
+	static public String COLOR_GRAY = "colorGray";
 
 }
