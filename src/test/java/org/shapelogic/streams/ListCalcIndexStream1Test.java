b2ab2d65443f3242f67a62e47c699df792f5a2e1
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2008-11-27 08:16:52 +0000

    Added new tests.

--- src/test/java/org/shapelogic/streams/ListCalcIndexStream1Test.java
+++ src/test/java/org/shapelogic/streams/ListCalcIndexStream1Test.java
@@ -1,8 +1,10 @@
 package org.shapelogic.streams;
 
 import org.shapelogic.calculation.CalcIndex1;
+import org.shapelogic.mathematics.NaturalNumberStream;
+import org.shapelogic.util.Constants;
 
-/** Test of TransformerStream.
+/** Test of ListCalcIndexStream1.
  * 
  * This is a lazy stream that will calculate a value based on 
  * 
@@ -12,19 +14,33 @@ import org.shapelogic.calculation.CalcIndex1;
  * <li>the other values in the stream</li> 
  * </ul>
  * 
- * XXX This should be called ListCalcIndexStream1Test or something close to that.<br />
- * 
  * @author Sami Badawi
  *
  */
 public class ListCalcIndexStream1Test extends AbstractListStreamTests {
+
+	ListCalcIndexStream1<Integer, Integer> even;
 
 	@Override
 	public void setUp() throws Exception {
 		super.setUp();
 		fibonacciNumbersAtStart = 2;
 		fibonacciNumbersAfterOneIteration = 2;
-	}
+
+		NaturalNumberStream natural = new NaturalNumberStream(3);
+		CalcIndex1<Integer, Integer> times2 = new CalcIndex1<Integer, Integer>() {
+
+			@Override
+            public Integer invoke(Integer input, int index) {
+				if (input == null)
+					return null;
+				return input + index;
+            }
+		};
+		even =
+			new ListCalcIndexStream1<Integer, Integer>(times2,(NumberedStream<Integer>)natural, Constants.LAST_UNKNOWN);
+        even.setNullLegalValue(false);
+    }
 
 	ListCalcIndexStream1<Object,Integer> countingBaseStreamFactory(final int stopNumber) {
 		ListCalcIndexStream1<Object, Integer> stream = new ListCalcIndexStream1<Object,Integer>() {
@@ -74,4 +90,55 @@ public class ListCalcIndexStream1Test extends AbstractListStreamTests {
 		return stream;
 	}
 	
+	public void testWithNext() {
+        assertEquals(Constants.LAST_UNKNOWN, even.getLast());
+		assertEquals(new Integer(0), even.next());
+		assertEquals(new Integer(2), even.next());
+		assertEquals(new Integer(4), even.next());
+		assertEquals(new Integer(6), even.next());
+//        assertEquals(Constants.LAST_UNKNOWN, even.getLast());
+//		assertFalse(even.hasNext());
+        assertEquals(3, even.getLast());
+		assertNull(even.next());
+	}
+
+	public void testWithGet() {
+		assertEquals(new Integer(0), even.get(0));
+		assertEquals(new Integer(2), even.get(1));
+		assertEquals(new Integer(4), even.get(2));
+		assertEquals(new Integer(6), even.get(3));
+		assertNull(even.get(4));
+	}
+
+
+	public void testGetTwiceWithSameResult() {
+		assertEquals(new Integer(0), even.get(0));
+		assertEquals(new Integer(2), even.get(1));
+		assertEquals(new Integer(4), even.get(2));
+		assertEquals(new Integer(6), even.get(3));
+		assertNull(even.get(4));
+		assertEquals(new Integer(0), even.get(0));
+		assertEquals(new Integer(2), even.get(1));
+		assertEquals(new Integer(4), even.get(2));
+		assertEquals(new Integer(6), even.get(3));
+		assertNull(even.get(4));
+	}
+
+	public void testIterateTwiceWithSameResult() {
+        assertEquals(Constants.LAST_UNKNOWN, even.getLast());
+		assertEquals(new Integer(0), even.get(0));
+		assertEquals(new Integer(2), even.get(1));
+		assertEquals(new Integer(4), even.get(2));
+		assertEquals(new Integer(6), even.get(3));
+//        assertEquals(Constants.LAST_UNKNOWN, even.getLast());
+		assertNull(even.get(4));
+        assertEquals(3, even.getLast());
+		assertEquals(new Integer(0), even.get(0));
+		assertEquals(new Integer(2), even.get(1));
+		assertEquals(new Integer(4), even.get(2));
+		assertEquals(new Integer(6), even.get(3));
+        assertEquals(3, even.getLast());
+		assertNull(even.get(4));
+        assertEquals(3, even.getLast());
+	}
 }
