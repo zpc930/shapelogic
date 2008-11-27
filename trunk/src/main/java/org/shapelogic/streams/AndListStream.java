34b86d0ce76ed9a0366663e82207d70c29dcb9fa
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2008-11-27 08:10:40 +0000

    Handle null input.

--- trunk/src/main/java/org/shapelogic/streams/AndListStream.java
+++ trunk/src/main/java/org/shapelogic/streams/AndListStream.java
@@ -11,8 +11,10 @@ public class AndListStream extends BaseListStreamList<Boolean,Boolean> {
 
 	@Override
 	public Boolean invoke(List<Boolean> input) {
+        if (input == null)
+            return true; // Neutral element for And
 		for (Boolean bool : input)
-			if (!bool)
+			if (bool != null && !bool)
 				return false;
 		return true;
 	}
