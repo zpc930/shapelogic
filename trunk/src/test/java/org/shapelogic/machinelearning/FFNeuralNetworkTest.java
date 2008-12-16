13bf34f825b4ba45ac843aaf9064ba77928ac4f6
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2008-12-16 18:00:44 +0000

    Started ShapeLogic 1.5 development, with machine learning and neural networks.

---
+++ trunk/src/test/java/org/shapelogic/machinelearning/FFNeuralNetworkTest.java
@@ -0,0 +1,16 @@
+package org.shapelogic.machinelearning;
+
+import junit.framework.TestCase;
+
+/** Test of Feed Forward Neural Network with external training. <br />
+ *
+ * The bias is considered the zeroth element of the synaptic weight.
+ *
+ * @author Sami Badawi
+ */
+public class FFNeuralNetworkTest extends TestCase {
+    
+    public void test() {
+        assertTrue(true);
+    }
+}
