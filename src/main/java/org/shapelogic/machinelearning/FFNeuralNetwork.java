285d059389c8ec92e106e225eabdfb422ac2843e
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2008-12-16 21:43:32 +0000

    Started work on XOR Neural Network test.

--- src/main/java/org/shapelogic/machinelearning/FFNeuralNetwork.java
+++ src/main/java/org/shapelogic/machinelearning/FFNeuralNetwork.java
@@ -10,14 +10,15 @@ import java.util.ArrayList;
  */
 public class FFNeuralNetwork {
 
-    protected int _nInput;
-    protected int _nOutput;
+    final public int nInputNodes;
+    final public int nOutputNodes;
     protected ArrayList<Integer> _layerNodes = new ArrayList();
     protected ArrayList<double[]> _layerWeights = new ArrayList();
+    protected String _message;
 
     public FFNeuralNetwork(int nInput, int nOutput) {
-        _nInput = nInput;
-        _nOutput = nOutput;
+        nInputNodes = nInput;
+        nOutputNodes = nOutput;
         _layerNodes.add(nInput);
         _layerWeights.add(new double[0]);
     }
@@ -26,7 +27,35 @@ public class FFNeuralNetwork {
         return input;
     }
 
-    public boolean loadLayer(double[] layer) {
+    public boolean addLayer(double[] layer) {
+//        if (_layerWeights.size() == 0) {}
+        int lastNumberOfNodes = _layerNodes.get(_layerNodes.size()-1);
+        int currentNumberOfNodes = layer.length / (lastNumberOfNodes+1);
+        _layerWeights.add(layer);
+        _layerNodes.add(currentNumberOfNodes);
+        if (layer.length % lastNumberOfNodes != 0)
+            _message = "Error number of weights for a layer = " +
+                    "(1 + nodes in last) * (nodes in current)\n" +
+                    "Values found \n" +
+                    "number of weight in layer = " + layer.length +
+                    "\nnumber of nodes in last layer = " + lastNumberOfNodes;
         return true;
+    }
+
+//=============== Getters and setters ===============
+    public ArrayList<Integer> getLayerNodes() {
+        return _layerNodes;
+    }
+
+    public int getLayerNodesInTopLayer() {
+        return _layerNodes.get(_layerNodes.size()-1);
+    }
+
+    public ArrayList<double[]> getLayerWeights() {
+        return _layerWeights;
+    }
+
+    public String getMessage() {
+        return _message;
     }
 }
