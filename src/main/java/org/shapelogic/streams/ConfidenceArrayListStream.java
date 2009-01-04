8a83096d45a61f8d065ab11ed02aa56b0cce6568
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2009-01-04 00:44:55 +0000

    Change to use Calc1: ConfidenceArraySelector

--- src/main/java/org/shapelogic/streams/ConfidenceArrayListStream.java
+++ src/main/java/org/shapelogic/streams/ConfidenceArrayListStream.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.shapelogic.calculation.Calc1;
 import org.shapelogic.calculation.RecursiveContext;
+import org.shapelogic.machinelearning.ConfidenceArraySelector;
 import org.shapelogic.util.Constants;
 
 /** ConfidenceArrayListStream takes a ListStream of double[] and creates a
@@ -16,7 +17,6 @@ import org.shapelogic.util.Constants;
  *
  */
 public class ConfidenceArrayListStream extends NamedListCalcStream1<double[], String>
-    implements RecursiveContext, Calc1<double[], String>
 {
 	
 	/** Parallel to the NumberedStream. */
@@ -30,31 +30,12 @@ public class ConfidenceArrayListStream extends NamedListCalcStream1<double[], St
 	 */
 	public ConfidenceArrayListStream(List<String> ohNames, String inputName,
             RecursiveContext recursiveContext, String outputName, int maxLast) {
-		super(null, inputName, recursiveContext, outputName, maxLast);
+		super(new ConfidenceArraySelector(ohNames), inputName, recursiveContext, outputName, maxLast);
 		_ohNames = ohNames;
-        setCalc(this);
 	}
 
-    @Override
-	public String invoke(double[] input) {
-        int countOK = 0;
-        int lastFound = -1;
-        for (int i = 0; i < input.length; i++) {
-            if (_limit < input[i]) {
-                countOK++;
-                lastFound = i;
-            }
-        }
-        if (countOK == 1) {
-            if (_ohNames != null && lastFound < _ohNames.size() )
-                return _ohNames.get(lastFound);
-            else
-                return "" + lastFound;
-        }
-        return Constants.NO_OH;
-    }
-
     public void setOhNames(List<String> ohNames) {
+        _calc = new ConfidenceArraySelector(ohNames);
         _ohNames = ohNames;
     }
 
