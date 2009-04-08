525a47fa8dbc62c93f88ea33245cb35ec39475b4
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2009-04-08 21:07:42 +0000

    Test greter than and smaller than neural network in stream.

--- trunk/src/test/java/org/shapelogic/machinelearning/ExampleNeuralNetworkTest.java
+++ trunk/src/test/java/org/shapelogic/machinelearning/ExampleNeuralNetworkTest.java
@@ -5,9 +5,6 @@ import org.shapelogic.calculation.RecursiveContext;
 import org.shapelogic.calculation.SimpleRecursiveContext;
 import org.shapelogic.mathematics.NumericTruthTableStream;
 import org.shapelogic.streams.ListStream;
-import org.shapelogic.util.Constants;
-import static org.shapelogic.machinelearning.FFNeuralNetworkTest.WEIGHTS_FOR_NOT;
-import static org.shapelogic.machinelearning.FFNeuralNetworkTest.WEIGHTS_FOR_XOR;
 
 /** Test of ExampleNeuralNetwork Stream with external training. <br />
  *
@@ -17,10 +14,9 @@ import static org.shapelogic.machinelearning.FFNeuralNetworkTest.WEIGHTS_FOR_XOR
  */
 public class ExampleNeuralNetworkTest extends TestCase {
     final static String ONE = "1";
-    final static String TWO = "2";
     final static String RESULT = "RESULT";
     final static String NOTHING = "";
-    final static String[] INPUT_ARRAY = { ONE, TWO };
+    final static String[] INPUT_ARRAY = { ONE };
     final static String[] RESULT_ARRAY = {RESULT};
 
     RecursiveContext _recursiveContext;
@@ -30,10 +26,8 @@ public class ExampleNeuralNetworkTest extends TestCase {
     @Override
     public void setUp() {
         _recursiveContext = new SimpleRecursiveContext(null);
-        numericTruthTableStream1 = new NumericTruthTableStream(1);
-        numericTruthTableStream2 = new NumericTruthTableStream(2,true);
+        numericTruthTableStream1 = new NumericTruthTableStream(1,true);
         _recursiveContext.getContext().put(ONE, numericTruthTableStream1);
-        _recursiveContext.getContext().put(TWO, numericTruthTableStream2);
     }
 
 
@@ -85,22 +79,21 @@ public class ExampleNeuralNetworkTest extends TestCase {
 
 //======================XOR NN======================
 
-    public void testXOrNeuralNetworkStream() {
+    public void testGreterThanNeuralNetworkStream() {
         FFNeuralNetworkStream nn = new FFNeuralNetworkStream(INPUT_ARRAY,
-                RESULT_ARRAY , WEIGHTS_FOR_XOR, _recursiveContext);
+                RESULT_ARRAY , ExampleNeuralNetwork.makeGreaterThanNeuralNetwork(0.8),
+                _recursiveContext);
         ListStream<String> output = nn.getOutputStream();
         assertEquals( NOTHING, output.next());
         assertEquals( RESULT, output.next());
-        assertEquals( RESULT, output.next());
-        assertEquals( NOTHING, output.next());
     }
 
 //======================NOT NN======================
 
-    public void testNotNeuralNetworkStream() {
-        FFNeuralNetworkStream nn = new FFNeuralNetworkStream(new String[] {ONE},
-                RESULT_ARRAY , WEIGHTS_FOR_NOT, null, _recursiveContext,
-                Constants.LAST_UNKNOWN);
+    public void testSmallerThanNeuralNetworkStream() {
+        FFNeuralNetworkStream nn = new FFNeuralNetworkStream(INPUT_ARRAY,
+                RESULT_ARRAY , ExampleNeuralNetwork.makeSmallerThanNeuralNetwork(0.8),
+                _recursiveContext);
         ListStream<String> output = nn.getOutputStream();
         assertEquals( RESULT, output.next());
         assertEquals( NOTHING, output.next());
