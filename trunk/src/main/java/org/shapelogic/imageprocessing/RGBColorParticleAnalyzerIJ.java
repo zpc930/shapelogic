57cd3bfd20cd292e696fae527072c7d75876ae51
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2009-05-07 03:43:22 +0000

    Add file dialog for config files.

--- trunk/src/main/java/org/shapelogic/imageprocessing/RGBColorParticleAnalyzerIJ.java
+++ trunk/src/main/java/org/shapelogic/imageprocessing/RGBColorParticleAnalyzerIJ.java
@@ -2,6 +2,7 @@ package org.shapelogic.imageprocessing;
 import ij.IJ;
 import ij.ImagePlus;
 import ij.gui.GenericDialog;
+import ij.io.OpenDialog;
 import ij.plugin.filter.PlugInFilterRunner;
 
 import org.shapelogic.color.ColorUtil;
@@ -41,6 +42,7 @@ public class RGBColorParticleAnalyzerIJ extends ColorParticleAnalyzerIJ {
         _gd.addCheckbox("ToMask: ", _toMaskStatic);
         _gd.addCheckbox("DisplayInternalInfo: ", _displayInternalInfoStatic);
         _gd.addCheckbox("UseNeuralNetwork: ", _useNeuralNetworkStatic);
+        _gd.addCheckbox("ShowFileDialog", false);
         _gd.addStringField("NeuralNetworkFile", _neuralNetworkFileStatic, 50); 
         
         _gd.showDialog();
@@ -66,7 +68,16 @@ public class RGBColorParticleAnalyzerIJ extends ColorParticleAnalyzerIJ {
         _toMask = _toMaskStatic = _gd.getNextBoolean();
         _displayInternalInfo = _displayInternalInfoStatic = _gd.getNextBoolean();
         _useNeuralNetwork = _useNeuralNetworkStatic = _gd.getNextBoolean();
-        _neuralNetworkFile = _neuralNetworkFileStatic = _gd.getNextString();
+        boolean showFileDialog = _gd.getNextBoolean();
+        String tempFilePath = null;
+        if (showFileDialog) {
+            OpenDialog od = new OpenDialog("Choose a .txt config file", null);
+            tempFilePath = getFilePath(od);
+            if (tempFilePath != null)
+                _neuralNetworkFile = _neuralNetworkFileStatic = tempFilePath;
+        }
+        if (tempFilePath == null)
+            _neuralNetworkFile = _neuralNetworkFileStatic = _gd.getNextString();
         return IJ.setupDialog(imp, _setupReturnValue);
 	}
 
