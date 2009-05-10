b32fd398d5f42f01b6814e91cb628bf16155657e
sami.badawi <sami.badawi@35684abd-4f3b-0410-98a5-33b4349e6bf1>
2009-05-10 04:45:51 +0000

    Changed stream name.

--- src/main/java/org/shapelogic/streamlogic/StreamNames.java
+++ src/main/java/org/shapelogic/streamlogic/StreamNames.java
@@ -1,7 +1,12 @@
 package org.shapelogic.streamlogic;
 
-/** Class with definitions common stream names.
- * 
+/** Class with definitions common stream names.<br />
+ *
+ * First strategy was not to used the same headlines and stream names to make it
+ * easier to debug in case of an error.<br />
+ *
+ * This has been dropped in order to make it simpler for the users.<br />
+ *
  * @author Sami Badawi
  *
  */
@@ -10,6 +15,7 @@ public class StreamNames {
 	public static final String POLYGONS = "polygons";
 	public static final String LETTERS = "letters";
 	public static final String PARTICLES = "particles";
+	public static final String CATEGORY = "Category"; //Same as headline
 	static public String COLOR_R = "colorR";
 	static public String COLOR_G = "colorG";
 	static public String COLOR_B = "colorB";
